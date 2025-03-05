34a6a96fa97c86bd93663e610cb59f5176b01644
lchangha <yullraes@gmail.com>
2025-03-05 16:26:47 +0900

    fix: 구현체에 어노테이션 안붙인거 수정...

--- src/main/java/io/traveler/travel/trip/service/TripService.java
+++ src/main/java/io/traveler/travel/trip/service/TripService.java
@@ -2,6 +2,5 @@ package io.traveler.travel.trip.service;
 
 import org.springframework.stereotype.Service;
 
-@Service
 public interface TripService {
 }
--- src/main/java/io/traveler/travel/trip/service/impl/TripServiceImpl.java
+++ src/main/java/io/traveler/travel/trip/service/impl/TripServiceImpl.java
@@ -1,6 +1,8 @@
 package io.traveler.travel.trip.service.impl;
 
-import io.traveler.travel.trip.service.TripService;
+import org.springframework.stereotype.Service;
 
+import io.traveler.travel.trip.service.TripService;
+@Service     
 public class TripServiceImpl implements TripService {
 }
