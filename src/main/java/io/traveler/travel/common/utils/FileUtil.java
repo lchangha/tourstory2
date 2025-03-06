cfaa99eeacbe638627809f2a6b54c80b02342d27
changha <lchangha0812@gmail.com>
2025-03-07 03:01:27 +0900

    fix: 중복 메서드 클래스화

---
+++ src/main/java/io/traveler/travel/common/utils/FileUtil.java
@@ -0,0 +1,21 @@
+package io.traveler.travel.common.utils;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public class FileUtil {
+
+    private FileUtil() {}
+
+    public static byte[] transferImageToBytes(MultipartFile file) {
+        if (file == null || file.isEmpty()) {
+            return new byte[0]; // 빈 파일 처리
+        }
+        try {
+            return file.getBytes();
+        } catch (IOException e) {
+            throw new RuntimeException("파일 변환 중 오류 발생", e);
+        }
+    }
+}
